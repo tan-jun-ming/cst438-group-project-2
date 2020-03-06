5cd4691d0c1abd1eba60a74cf798cdfc8d4c0bf5
Dane1247 <42821621+Dane1247@users.noreply.github.com>
2020-03-05 16:21:27 -0800

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgres://adminUser:adminPass@localhost/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgres://postgres@localhost/postgres"
